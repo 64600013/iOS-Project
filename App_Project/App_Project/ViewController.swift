4399e0f8bd5888c6b2c1a2f13f1f64ece4bcecc9
64600013 <321654014f@gmail.com>
2022-01-07 02:53:02 -0800

    Initial app build

---
+++ App_Project/App_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_Project
+//
+//  Created by outis on 7/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
