431172b04e65ffc4e793bfb9b5fa247c54cf40d9
64600013 <321654014f@gmail.com>
2022-01-11 21:08:21 +0800

    ui improvements

--- AttractionApp/ViewController.swift
+++ AttractionApp/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Attraction App"
         // Do any additional setup after loading the view.
     }
 
