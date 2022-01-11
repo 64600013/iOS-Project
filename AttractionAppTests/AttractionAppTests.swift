4f9ffbbe9230abdd7be930da41244b03068a7538
64600013 <321654014f@gmail.com>
2022-01-11 16:14:51 +0800

    rename the other missing files

--- AttractionAppTests/AttractionAppTests.swift
+++ AttractionAppTests/AttractionAppTests.swift
@@ -1,9 +1,9 @@
 
 
 import XCTest
-@testable import LocationEx
+@testable import AttractionApp
 
-class LocationExTests: XCTestCase {
+class AttractionAppExTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
