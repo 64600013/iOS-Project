a03066213d3b5dcb7a46ef4e7a394f4e12be09cd
64600013 <321654014f@gmail.com>
2022-01-12 14:55:30 +0800

    finished table view

--- AttractionApp/CitiesViewController.swift
+++ AttractionApp/CitiesViewController.swift
@@ -9,9 +9,8 @@ class CitiesViewController: UIViewController,UITableViewDataSource {
 //    var annotations = [MKPointAnnotation]();
     let attractionArray: [[String]] = [["Ocean Park", "The Peak", "The Sky100", "Disneyland", "Lan Kwai Fong"], ["The Buddha", "The Wong Tai Sin Temple", "Man Mo Temple", "Po Lin Monastery"], ["Star Ferry", "Tai O Fishing Village", "Temple Street Night Market"]]
     let headers: [String] = ["Modern Attractions", "Traditional Temples", "Old Hong Kong Sites"]
-    let picsArray : [UIImage] = []
-    
-    
+    let picsArray: [[UIImage]] = [[#imageLiteral(resourceName: "oceanpark"), #imageLiteral(resourceName: "peak"), #imageLiteral(resourceName: "sky100"), #imageLiteral(resourceName: "disneyland"), #imageLiteral(resourceName: "lkf")],[#imageLiteral(resourceName: "buddha"), #imageLiteral(resourceName: "wts"), #imageLiteral(resourceName: "manwo"), #imageLiteral(resourceName: "polin")],[#imageLiteral(resourceName: "starferry"), #imageLiteral(resourceName: "taio"), #imageLiteral(resourceName: "templestreet")]]
+    let details: [[String]] = [["Ocean Park Road, Aberdeen, Hong Kong Island", "128 Peak Road, Mid-Levels, Hong Kong Island", "100/F, International Commerce Centre, 1 Austin Road West, Kowloon", "Sunny Bay, Lantau Island", "1 Lan Kwai Fong, Central"], ["Ngong Ping Rd, Lantau Island", "Sik Sik Yuen Wong Tai Sin Temple, 2, Chuk Yuen Village, Wong Tai Sin, Kowloon, Hong Kong", "Man Mo Temple, Hollywood Rd, Sheung Wan", "Po Lin Monastery, Ngong Ping, Lantau Island"], ["Star Ferry Pier, Tsim Sha Tsui, Central or Wan Chai", "Tai O, Lantau Island", "Temple St, Jordan, Kowloon"]]
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return attractionArray.count
@@ -28,12 +27,15 @@ class CitiesViewController: UIViewController,UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let displayCell = tableView.dequeueReusableCell(withIdentifier: "displayCell", for: indexPath)
         displayCell.textLabel?.text = attractionArray[indexPath.section][indexPath.row]
+        displayCell.imageView?.image = picsArray[indexPath.section][indexPath.row]
+        displayCell.detailTextLabel?.text = details[indexPath.section][indexPath.row]
         return displayCell
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Attraction Address"
 //        let nycAnnotation = MKPointAnnotation();
 //        nycAnnotation.coordinate = CLLocationCoordinate2D(latitude: 40.71, longitude: -74.0);
 //         nycAnnotation.title = "New York City";
