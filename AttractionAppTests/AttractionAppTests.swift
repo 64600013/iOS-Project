808e4392697af9a9059c18d0107a7d7f5d150195
64600013 <321654014f@gmail.com>
2022-01-13 16:54:02 +0800

    add more test

--- AttractionAppTests/AttractionAppTests.swift
+++ AttractionAppTests/AttractionAppTests.swift
@@ -1,5 +1,5 @@
 
-
+import MapKit
 import XCTest
 @testable import AttractionApp
 
@@ -13,9 +13,28 @@ class AttractionAppExTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    //this test the data saving function
     func testSaveData() throws {
         let data = AttractionFinderViewController()
+        //enter
+        let stringEnterMsg = "The User have entered 4 times"
         data.saveEnterData(number: 4)
+        XCTAssertEqual(data.saveMsg, stringEnterMsg)
+        
+        //exit
+        let stringExitMsg = "The User have exited 4 times"
+        data.saveExitData(number: 4)
+        XCTAssertEqual(data.saveMsg, stringExitMsg)
+    }
+    
+    //This test the pin adding function
+    func testPins() throws {
+        let data = AttractionFinderViewController()
+        let testAnnotation = MKPointAnnotation();
+        testAnnotation.coordinate = CLLocationCoordinate2D(latitude: 22.384389, longitude: 114.288950);
+        testAnnotation.title = "Test Site";
+        data.annotations.append(testAnnotation);
+        
     }
     
     func testPerformanceExample() throws {
